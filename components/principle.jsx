9c2c90aa3dce10f8d95e5eb690eb8b089bd676bd
muzammil-15 <muzammilcoder15@gmail.com>
2023-09-25 23:08:22 +0500

    priciple quote change

--- components/principle.jsx
+++ components/principle.jsx
@@ -15,7 +15,7 @@ const Principle = () => {
         <div className="lg:w-1/2 lg:pl-4 mt-4 lg:mt-0">
           <blockquote class="text-xl italic leading-10 font-semibold text-gray-900 ">
             <p>
-            "Dear students, your college journey is a chapter in the book of your life, and I urge you to make it a memorable one. Remember, success is not measured by the grades you earn but by the knowledge you gain. Embrace challenges, for they are stepping stones to growth. Cherish friendships, as they will be your support system. Stay curious, as learning never ends. And always remember, your potential is limitless. As your college principal, I have faith in your abilities, and I believe you can achieve greatness. So, dream big, work hard, and make us proud. The future is yours to shape!"
+            Dear students, your college journey is a chapter in the book of your life, and I urge you to make it a memorable one. Remember, success is not measured by the grades you earn but by the knowledge you gain. Embrace challenges, for they are stepping stones to growth. Cherish friendships, as they will be your support system. Stay curious, as learning never ends. And always remember, your potential is limitless. As your college principal, I have faith in your abilities, and I believe you can achieve greatness. So, dream big, work hard, and make us proud. The future is yours to shape!
             </p>
           </blockquote>
         </div>
